4b9433f07db2f6e46ebf09e0840dc9423281f1f3
Brian Gibson <bggibson@hotmail.com>
2018-12-13 00:52:01 -0500

    Adds version task and add as dependency to default task in jake

--- Jakefile.js
+++ Jakefile.js
@@ -1,8 +1,20 @@
 (function () {
     "use strict";
 
+    var EXPECTED_NODE_VERSION = "v11.4.0";
+
     desc("Default build");
-    task("default", function () {
+    task("default", [ "version" ], function () {
         console.log("\n\nBUILD OK");
     });
+
+    desc("Check node version");
+    task("version", function () {
+        console.log("Checking node version: .");
+
+        var actualVersion = process.version;
+        if (actualVersion !== EXPECTED_NODE_VERSION)
+            fail("Incorrect Node version: expected " + EXPECTED_NODE_VERSION + ", but was " + actualVersion)
+    });
+
 }());
