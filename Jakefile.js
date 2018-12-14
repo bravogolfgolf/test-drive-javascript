487f0cc3257c43a45f36ece04d434327e9ab684b
Brian Gibson <bggibson@hotmail.com>
2018-12-14 15:07:37 -0500

    Adds Integration task

--- Jakefile.js
+++ Jakefile.js
@@ -5,7 +5,20 @@
     var semver = require("semver");
     var jshint = require("simplebuild-jshint");
 
-    desc("Default build");
+    desc("Integrate");
+    task("integrate", ["default"], function() {
+        console.log("1. Make sure 'git status' is clean.");
+        console.log("2. Build on the integration box.");
+        console.log("   a. Walk over to integration box.");
+        console.log("   b. 'git pull'");
+        console.log("   c. 'jake'");
+        console.log("   d. If jake fails, stop! Try again after fixing the issue.");
+        console.log("3. 'git checkout integration'");
+        console.log("4. 'git merge master --no-ff --log'");
+        console.log("5. 'git checkout master'");
+    });
+
+    desc("Build and test");
     task("default", ["version", "lint"], function () {
         console.log("\n\nBUILD OK");
     });
