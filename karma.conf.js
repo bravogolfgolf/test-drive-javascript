0b1370cf97cbcf4b3e6b31e73691035f284a2ebb
Brian Gibson <bggibson@hotmail.com>
2018-12-15 00:15:49 -0500

    Installs chai as thrid-party dependency

--- karma.conf.js
+++ karma.conf.js
@@ -1,70 +1,71 @@
 // Karma configuration
 // Generated on Fri Dec 14 2018 16:31:38 GMT-0500 (Eastern Standard Time)
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'commonjs'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      'src/**/*.js'
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['mocha', 'commonjs'],
 
 
-    // list of files / patterns to exclude
-    exclude: [
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            'src/js/**/*.js',
+            'src/third-party/chai-4.2.0.js'
+        ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'src/**/*.js':['commonjs']
-    },
+        // list of files / patterns to exclude
+        exclude: [],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots'],
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'src/js/**/*.js': ['commonjs'],
+            'src/third-party/chai-4.2.0.js': ['commonjs']
+        },
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['dots'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: [],
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity
+    })
 }
--- src/js/arithmetic_test.js
+++ src/js/arithmetic_test.js
@@ -1,16 +1,12 @@
 (function () {
     "use strict";
 
+    var assert = require("../third-party/chai-4.2.0.js").assert;
     var arithmetic = require("./arithmetic.js");
 
     describe("Arithmetic", function () {
         it("adds two numbers", function () {
-            assertEquals(arithmetic.add(3, 4), 7);
+            assert.equal(arithmetic.add(3, 4), 7);
         });
     });
-
-    function assertEquals(actual, expected) {
-        if (actual !== expected) throw new Error("Actual was" + actual + ", but expected: " + expected);
-
-    }
 }());
