99a1a21ba178ceaee1faf55d33bedec644147e22
Brian Gibson <bggibson@hotmail.com>
2018-12-16 17:07:39 -0500

    Test server responds to get request

--- src/js/server/server_test.js
+++ src/js/server/server_test.js
@@ -3,11 +3,26 @@
 
     var assert = require("../shared/assert.js");
     var server = require("./server.js");
+    var http = require("http");
+
 
     describe('Server should', function () {
-        it('should return number', function () {
-            assert.equal(3 , server.number());
+
+        beforeEach(function (done) {
+            server.start();
+            done();
+        });
+
+        afterEach(function (done) {
+            server.stop();
+            done();
+        });
+
+        it('respond to get request', function (done) {
+            http.get({protocol: "http:", host: "localhost", port: 8080}, function (response) {
+                assert.equal(response.statusCode, 200);
+                done();
+            });
         });
     });
-
 }());
