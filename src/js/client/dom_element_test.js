8384956bd415ab7fa413b6a9ae8215a3dd8dfac5
Brian Gibson <bggibson@hotmail.com>
2018-12-28 19:04:05 -0500

    Completes testing of dom_element mouse and single touch events

--- src/js/client/dom_element_test.js
+++ src/js/client/dom_element_test.js
@@ -1,13 +1,50 @@
+/* globals wwp:true, TouchEvent:false */
+
+
 (function () {
     "use strict";
 
     var assert = require("../shared/assert");
 
-    describe("", function () {
+    describe("dom_element should have", function () {
+        var domElement;
 
-        it("", function () {
-            assert.isOk(true);
+        beforeEach(function () {
+            var div = document.createElement("div");
+            domElement = new wwp.DomElement(div);
         });
 
+        describe("mouse events", function () {
+            it("handled on all devices", function () {
+                testEvent(domElement.onMouseDown, domElement.doMouseDown);
+                testEvent(domElement.onMouseMove, domElement.doMouseMove);
+                testEvent(domElement.onMouseLeave, domElement.doMouseLeave);
+                testEvent(domElement.onMouseUp, domElement.doMouseUp);
+            });
+
+        });
+
+        describe("touch events", function () {
+            if (!browserSupportsTouchEvents()) return;
+            it("handled on supported devices", function () {
+                testEvent(domElement.onSingleTouchStart, domElement.doSingleTouchStart);
+                testEvent(domElement.onSingleTouchMove, domElement.doSingleTouchMove);
+                testEvent(domElement.onSingleTouchCancel, domElement.doSingleTouchCancel);
+                testEvent(domElement.onSingleTouchEnd, domElement.doSingleTouchEnd);
+            });
+        });
+
+        function testEvent(listener, instigator) {
+            var actual = null;
+            listener.call(domElement, function (point) {
+                actual = point;
+            });
+            instigator.call(domElement, 123, 456);
+            assert.deepEqual(actual, {x: 123, y: 456});
+        }
+
+        function browserSupportsTouchEvents() {
+            return 'TouchEvent' in window && TouchEvent.length > 0;
+        }
     });
 }());
